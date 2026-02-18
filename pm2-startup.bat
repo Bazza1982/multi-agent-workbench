9b7be60bd408b012407a196c2c57e02bf5fed2fa
Bazza <barrytianli@gmail.com>
2026-02-18 17:38:15 +1100

    feat: auto open browser on startup

--- pm2-startup.bat
+++ pm2-startup.bat
@@ -1,6 +1,14 @@
 @echo off
 :: PM2 Startup Script for Windows
-:: Run this at system startup to restore PM2 processes
+:: Run this at system startup to restore PM2 processes and open browser
 
 cd /d "%~dp0"
+
+:: Start PM2 processes
 pm2 resurrect
+
+:: Wait for services to start (5 seconds)
+timeout /t 5 /nobreak >nul
+
+:: Open browser to Workbench
+start "" "http://localhost:5173"
